d36065e40ea8f974f6205e8adf32575065701618
unknown <j4yn3rd@gmail.com>
2018-05-11 16:12:29 +1200

    refac

--- src/Queen.py
+++ src/Queen.py
@@ -1,13 +1,29 @@
 from src.Spider import Spider
 
-spider = Spider()
 
-spider.scrap("a", "Navigation")
-qualification_types = spider.get_entries()
-spider.refine_urls()
-print(qualification_types)
+class Queen:
 
-spider.scrap("a", "Navigation")
-qualifications = spider.get_entries()
-spider.refine_urls()
-print(qualifications)
+    def __init__(self):
+        self.spider = Spider()
+
+    def dispatch_spider(self, tag_a, tag_b):
+        self.spider.scrap(tag_a, tag_b)
+
+    def collect_information(self):
+        brain = self.spider.get_entries()
+        return brain
+
+    def washout_spider(self):
+        self.spider.clean_up_urls()
+
+
+queen = Queen()
+
+queen.dispatch_spider("a", "Navigation")
+queen.washout_spider()
+
+queen.dispatch_spider("a", "Navigation")
+queen.washout_spider()
+
+queen.dispatch_spider("a", "Navigation")
+queen.washout_spider()
--- src/Spider.py
+++ src/Spider.py
@@ -10,38 +10,34 @@ class Spider:
     def __init__(self):
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         self.http = urllib3.PoolManager()
-        self.given_urls = []
+        self.target_urls = []
         self.scraped_urls = []
         self.scraped_entries = []
         self.base_url = base_url
-        self.given_urls.append(initial_url)
+        self.target_urls.append(initial_url)
 
     # Start scraping
     def scrap(self, tag_a, tag_b):
-        # Scraping inner urls
-        for url in self.given_urls:
+        # Scraping nested urls
+        for url in self.target_urls:
             request = self.http.request("GET", base_url + url)
             soup = BeautifulSoup(request.data, "lxml")
             for result in soup.find_all(tag_a, tag_b):
                 self.scraped_urls.append(result)
 
-        # Scraping entry texts
-        for tag in self.scraped_urls:
-            self.scraped_entries.append(tag.text)
-
-    # Returns collected entries to the queen
+    # Returns scraped entries to the queen
     def get_entries(self):
         target_entries = []
-        for entry in self.scraped_entries:
-            target_entries.append(entry)
+        for tag in self.scraped_urls:
+            target_entries.append(tag.text)
         return target_entries
 
     # Refines collected urls to working links without unnecessary tags
     # Clears values from previous extractions to avoid any duplicates
-    def refine_urls(self):
-        self.given_urls.clear()
+    def clean_up_urls(self):
+        self.target_urls.clear()
         self.scraped_entries.clear()
         for url in self.scraped_urls:
             url = url.get("href")
-            self.given_urls.append(url)
+            self.target_urls.append(url)
         self.scraped_urls.clear()
